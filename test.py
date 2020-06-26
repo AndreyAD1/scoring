4257100252b3066d3629c2eb1eb001a4f962801c
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2020-06-27 00:21:15 +0300

    Uncomment negative tests.

--- test.py
+++ test.py
@@ -57,28 +57,28 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(api.INVALID_REQUEST, code)
         self.assertTrue(len(response))
 
-    # @cases([
-    #     {},
-    #     {"phone": "79175002040"},
-    #     {"phone": "89175002040", "email": "stupnikov@otus.ru"},
-    #     {"phone": "79175002040", "email": "stupnikovotus.ru"},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": -1},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": "1"},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.1890"},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "XXX"},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "first_name": 1},
-    #     {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
-    #      "first_name": "s", "last_name": 2},
-    #     {"phone": "79175002040", "birthday": "01.01.2000", "first_name": "s"},
-    #     {"email": "stupnikov@otus.ru", "gender": 1, "last_name": 2},
-    # ])
-    # def test_invalid_score_request(self, arguments):
-    #     request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
-    #     self.set_valid_auth(request)
-    #     response, code = self.get_response(request)
-    #     self.assertEqual(api.INVALID_REQUEST, code, arguments)
-    #     self.assertTrue(len(response))
-    #
+    @cases([
+        # {},
+        # {"phone": "79175002040"},
+        {"phone": "89175002040", "email": "stupnikov@otus.ru"},
+        {"phone": "79175002040", "email": "stupnikovotus.ru"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": -1},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": "1"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.1890"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "XXX"},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000", "first_name": 1},
+        {"phone": "79175002040", "email": "stupnikov@otus.ru", "gender": 1, "birthday": "01.01.2000",
+         "first_name": "s", "last_name": 2},
+        # {"phone": "79175002040", "birthday": "01.01.2000", "first_name": "s"},
+        {"email": "stupnikov@otus.ru", "gender": 1, "last_name": 2},
+    ])
+    def test_invalid_score_request(self, arguments):
+        request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
+        self.set_valid_auth(request)
+        response, code = self.get_response(request)
+        self.assertEqual(api.INVALID_REQUEST, code, arguments)
+        self.assertTrue(len(response))
+
     @cases([
         {"phone": "79175002040", "email": "stupnikov@otus.ru"},
         {"phone": 79175002040, "email": "stupnikov@otus.ru"},
